9e7bc193a059d977b101fc09d1cff5323e18ef8e
Davide Wiest <66053288+DavideWiest@users.noreply.github.com>
2022-09-24 17:18:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ import time
 
 # username, password
 ACCOUNTS_DATA = [
-    ["seauser565", "seauser656"]
+    ["", ""]
 ]
 USERMAX = float("inf")
 SLEEP_TIME = 7
 LONG_SLEEP_TIME = (3600 * 0.5, 3600 * 2)
 ANALYZE_PREVENTION = ("sleep reconnect proxy_reconnect", 3600 * 2.5)
-GFL_FILTER = {"category": {"$nin": ["Artist", "Art", "Photographer", "Graphic Designer", "Visual Arts"]}}
+# GFL_FILTER = {"category": {"$in" ["Entrepreneur", "Public figure", "Product/service", "Real Estate Agent", "Retail company", "Local business", "Digital Creator"]}}
+# GFL_FILTER = {"category": {"$nin": ["Artist", "Art", "Photographer", "Graphic Designer", "Visual Arts"]}}
+GFL_FILTER = {}
 DB_RECONNECT_S = 3600 * 4
 
 if __name__ == "__main__":
@@ -28,6 +30,5 @@ if __name__ == "__main__":
 
     id = InstaData(ACCOUNTS_DATA, USERMAX, SLEEP_TIME, LONG_SLEEP_TIME, ANALYZE_PREVENTION, mm, ta, ls, dh, pc)
 
-    # GFL_FILTER = {"category": {"$in" ["Entrepreneur", "Public figure", "Product/service", "Real Estate Agent", "Retail company", "Local business", "Digital Creator"]}}
     id.make_list(use_log=True, db_reconnect=DB_RECONNECT_S, gfl_filter=GFL_FILTER)
 
